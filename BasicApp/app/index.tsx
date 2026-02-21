82dfb1adbd7e6239417b9b79045790f87afcd70c
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-22 00:24:02 +0100

    add Link and file based routing

--- BasicApp/app/index.tsx
+++ BasicApp/app/index.tsx
@@ -1,3 +1,4 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Home() {
@@ -7,9 +8,12 @@ export default function Home() {
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
+        gap: 10,
       }}
     >
       <Text>Home page</Text>
+      <Link href="/about">Go to about</Link>
+      <Link href="./profile">Go to profile</Link>
     </View>
   );
 }
