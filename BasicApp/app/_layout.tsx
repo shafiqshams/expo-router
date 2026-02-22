b6a5eb8b3e284dcfb3f80e9da3d873fa6a7c5478
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-22 02:19:08 +0100

    update root layout with header and footer

--- BasicApp/app/_layout.tsx
+++ BasicApp/app/_layout.tsx
@@ -1,5 +1,31 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Text style={{ color: "white" }}>Header</Text>
+      </View>
+      <Slot />
+      <View style={styles.footer}>
+        <Text style={{ color: "white" }}>Footer</Text>
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  header: {
+    backgroundColor: "red",
+    padding: 20,
+  },
+  footer: {
+    backgroundColor: "blue",
+    padding: 20,
+  },
+});
