8747045e4ae3da15f788cb86bcc5949745aa846b
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-23 23:54:47 +0100

    add relative path for product list

--- BasicApp/app/products/index.tsx
+++ BasicApp/app/products/index.tsx
@@ -6,9 +6,13 @@ export default function ProductList() {
   return (
     <View style={styles.container}>
       <Text>ProductList</Text>
-      <Link href="./products/1">Product 1</Link>
-      <Link href="./products/2">Product 2</Link>
-      <Link href="./products/3">Product 3</Link>
+
+      <Link href="./1" relativeToDirectory>
+        Product 1
+      </Link>
+
+      <Link href="./2">Product 2</Link>
+      <Link href="./3">Product 3</Link>
     </View>
   );
 }
