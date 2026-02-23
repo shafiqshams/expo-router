8bc0c64aa427a9126cd53377909152949718d579
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-23 23:52:22 +0100

    add stylesheet and remove inline styles

--- BasicApp/app/(app)/index.tsx
+++ BasicApp/app/(app)/index.tsx
@@ -1,16 +1,9 @@
 import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        gap: 10,
-      }}
-    >
+    <View style={styles.container}>
       <Text>Home page</Text>
       <Link href="/about">Go to about</Link>
       <Link href="./profile/missing">Go to profile</Link>
@@ -20,3 +13,21 @@ export default function Home() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    gap: 20,
+  },
+  button: {
+    backgroundColor: "#0ea5e9",
+    padding: 10,
+    borderRadius: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+  },
+});
