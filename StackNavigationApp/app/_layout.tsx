5ccb0354ff1c50491a8361b89634b59129516e4e
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-24 01:02:56 +0100

    add Stack screen with options

--- StackNavigationApp/app/_layout.tsx
+++ StackNavigationApp/app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerStyle: { backgroundColor: "#6a51ae" },
+          headerTintColor: "white",
+          headerTitleStyle: { fontWeight: "bold" },
+        }}
+      />
+      <Stack.Screen
+        name="about"
+        options={{
+          title: "About",
+        }}
+      />
+    </Stack>
+  );
 }
