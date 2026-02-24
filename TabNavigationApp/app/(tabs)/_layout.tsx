0c9c3a00528e00f3d284d74b1d5f2aa7d533412a
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-24 01:40:48 +0100

    add customizations to tab bar with icon and label

--- TabNavigationApp/app/(tabs)/_layout.tsx
+++ TabNavigationApp/app/(tabs)/_layout.tsx
@@ -1,5 +1,40 @@
+import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
 
 export default function TabsLayout() {
-  return <Tabs />;
+  return (
+    <Tabs>
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="home" color={color} size={24} />
+          ),
+          title: "Home",
+        }}
+      />
+      <Tabs.Screen
+        name="explore"
+        options={{
+          tabBarLabel: "Explore",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="search" color={color} size={24} />
+          ),
+          title: "Explore",
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          tabBarLabel: "Profile",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name="user" color={color} size={24} />
+          ),
+          tabBarBadge: 3,
+          title: "Home",
+        }}
+      />
+    </Tabs>
+  );
 }
