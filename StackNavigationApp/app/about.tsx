abd71056d925a65c1140d727dd72b326ea6d5999
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-24 01:02:07 +0100

    add about screen

---
+++ StackNavigationApp/app/about.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function About() {
+  return (
+    <View style={styles.container}>
+      <Text>About</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
