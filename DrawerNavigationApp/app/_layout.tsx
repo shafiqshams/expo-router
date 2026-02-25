39a8a245f16656e91e4aa7e09496e785478f0198
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-25 22:28:23 +0100

    add drawer styles with screen options

--- DrawerNavigationApp/app/_layout.tsx
+++ DrawerNavigationApp/app/_layout.tsx
@@ -4,7 +4,15 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Drawer>
+      <Drawer
+        screenOptions={{
+          headerStyle: { backgroundColor: "#6a51ae" },
+          headerTintColor: "white",
+          drawerStyle: { backgroundColor: "#6a51ae" },
+          drawerActiveTintColor: "white",
+          drawerInactiveTintColor: "#acacac",
+        }}
+      >
         <Drawer.Screen
           name="index"
           options={{
