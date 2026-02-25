61cf30a087ec0ad55ff8c7ae0765de8874ab9d22
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-25 22:25:35 +0100

    add drawer icons

--- DrawerNavigationApp/app/_layout.tsx
+++ DrawerNavigationApp/app/_layout.tsx
@@ -1,3 +1,4 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Drawer } from "expo-router/drawer";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 export default function RootLayout() {
@@ -8,18 +9,27 @@ export default function RootLayout() {
           name="index"
           options={{
             title: "Home",
+            drawerIcon: ({ color }) => (
+              <FontAwesome name="home" color={color} size={24} />
+            ),
           }}
         />
         <Drawer.Screen
           name="dashboard"
           options={{
             title: "Dashboard",
+            drawerIcon: ({ color }) => (
+              <FontAwesome name="dashboard" color={color} size={24} />
+            ),
           }}
         />
         <Drawer.Screen
           name="settings"
           options={{
             title: "Settings",
+            drawerIcon: ({ color }) => (
+              <FontAwesome name="gear" color={color} size={24} />
+            ),
           }}
         />
       </Drawer>
