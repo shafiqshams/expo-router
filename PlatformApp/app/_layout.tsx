f743ac1b418b0c3419f3c8979b40134d4ad1cf34
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-27 04:05:14 +0100

    add conditional rendering using Platform api

--- PlatformApp/app/_layout.tsx
+++ PlatformApp/app/_layout.tsx
@@ -1,5 +1,18 @@
-import { Stack } from "expo-router";
+import { Tabs } from "expo-router";
+import { Platform } from "react-native";
 
 export default function RootLayout() {
-  return <Stack />;
+  if (Platform.OS === "web") {
+    return (
+      <div style={{ display: "flex", gap: 20, padding: 20 }}>
+        <h1>Web Layout</h1>
+        <nav style={{ display: "flex", gap: 20 }}>
+          <a href="/">Home</a>
+          <a href="/about">About</a>
+        </nav>
+      </div>
+    );
+  }
+
+  return <Tabs />;
 }
