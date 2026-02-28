d18712c1305ff15d431ec72a2dcd5d8b94ce398d
Shafiq Shams <shafiqshamsessani@gmail.com>
2026-02-28 01:04:37 +0100

    add dummy list and link to profile

--- ExtendedTabsApp/app/index.tsx
+++ ExtendedTabsApp/app/index.tsx
@@ -1,15 +1,27 @@
-import { Text, View } from "react-native";
+import { Link } from "expo-router";
+import { Button, Image, ScrollView, Text, View } from "react-native";
 
 export default function Home() {
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <ScrollView >
+      <Link href="/profile" style={{ padding: 10 }}>
+        <Button title="Profile" />
+      </Link>
+
+      {[...Array(20)].map((_, i) => (
+        <View
+          key={`item-${i}`}
+          style={{ padding: 10, flexDirection: "row", alignItems: "center" }}
+        >
+          <Image
+            source={require("../assets/images/react-logo.png")}
+            style={{ width: 32, height: 32, marginRight: 16 }}
+          />
+
+          <Text>Dummy Item {i + 1}</Text>
+        </View>
+      ))}
+    </ScrollView>
   );
 }
